9802c04eaecd0dc5b6380b014d7a0049fbdcc79c
Jim <jdfban@gmail.com>
2022-02-08 10:39:15 +0000

    change

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const quote = document.querySelector('.quoteOpacity');
+const image1 = document.querySelector('.img1');
+const image2 = document.querySelector('.img2');
 let clicks = 0;
 
 function revealQuote() {
@@ -6,10 +8,18 @@ function revealQuote() {
     clicks++;
     quote.style.opacity = '1';
     quote.style.transition = '1s';
+    image1.style.opacity = '0';
+    image1.style.transition = '1s';
+    image2.style.opacity = '1';
+    image2.style.transition = '1s';
     }
     else if (clicks==1) {
         clicks--;
         quote.style.opacity = '0';
         quote.style.transition = '1s';
+        image2.style.opacity = '0';
+        image2.style.transition = '1s';
+        image1.style.opacity = '1';
+        image1.style.transition = '1s';
     }
 };
